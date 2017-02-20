2122ff0ed589d43814c3bc746e14679e6ce65380
Janaka Wijekoon <janaka@west.sd.keio.ac.jp>
2017-02-20 12:03:33 +0900

    Warning fixeed OpenSyslog (__FILE__, LOG_USER);

--- westSysLogC.h
+++ westSysLogC.h
@@ -10,7 +10,7 @@
 	* //Create a global char array 
 	* char message[100]; 
 	* 
-	* OpenSyslog (argv[0], LOG_USER); // Passing the program name as the syslog entry name
+	* OpenSyslog (argv[0], user); // Passing the program name as the syslog entry name
 	* sprintf (message, "This is from Thread:%d, I am executing on the CPU_ID:%d\n",10, 10);
 	* ToWestlabLog (debug, message); // Specify the log type. Note that this version distinguishs only DEBUG messages.
 																	 // All other types are referred to as INFO.
